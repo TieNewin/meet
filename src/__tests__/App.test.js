cf2179aa3c50a53049ec1240772595b5c271cf35
TieNewin <125307533+TieNewin@users.noreply.github.com>
2023-09-10 10:46:44 -0500

    exercise 4.5

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,21 +1,38 @@
-import { render } from '@testing-library/react';
 import App from '../App';
+import { render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getEvents } from '../api';
 
 describe('<App /> component', () => {
   let AppDOM;
-  beforeEach(() => {
-    AppDOM = render(<App />).container.firstChild;
-  })
-
-  test('renders list of events', () => {
-    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
-  });
-
-  test('render CitySearch', () => {
-    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
-  });
-
-  test('render number of events', () => {
     expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+  });
+
+describe('<App /> integration', () => {
+  test('renders a list of events matching the city selected by the user', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+    await userEvent.type(CitySearchInput, 'Berlin');
+    const berlinSuggestionItem =
+      within(CitySearchDOM).queryByText('Berlin, Germany');
+    await userEvent.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems =
+      within(EventListDOM).queryAllByRole('listitem');
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      (event) => event.location === 'Berlin, Germany'
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+    allRenderedEventItems.forEach((event) => {
+      expect(event.textContent).toContain('Berlin, Germany');
+    });
   });
 });
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,25 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
-
   return (
-    <li>
-      <h2>{event.summary}</h2>
-      <p>{event.created}</p>
-      <p>{event.location}</p>
-      <button onClick={() => {setShowDetails(!showDetails)}}>
-        {showDetails ? 'Hide Details' : 'Show Details'}
-      </button>
-      {showDetails ? (
-        <div className="details">
-          <h3>Details</h3>
-          <p>Description: {event.description}</p>
-          <p>Status: {event.status}</p>
-        </div>
-      ) : null}
+    <li className="event">
+      <h2>{event && event.summary}</h2>
+      <p>{event && event.location}</p>
+      <p>{event && (new Date(event.created)).toUTCString()}</p>
+      {showDetails ?
+        <p className="details">{event && event.description}</p> :
+        null
+      }
+      <button className="details-btn" onClick={() => {
+        showDetails ? setShowDetails(false) : setShowDetails(true)
+      }}>{showDetails ? "hide details" : "show details"}</button>
     </li>
-  );
+  )
 }
 
 export default Event;
